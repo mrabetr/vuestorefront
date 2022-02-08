bf0a7a617bc0111277c67e3315c6cd624b2f25aa
rafik mrabet <mrabetr@hotmail.com>
2022-02-08 22:04:18 +0000

    replaced project name instances

--- packages/theme/middleware.config.js
+++ packages/theme/middleware.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   integrations: {
-    __replace_me__: {
-      location: '@vue-storefront/__replace_me__-api/server',
+    vuestorefront: {
+      location: '@vue-storefront/vuestorefront-api/server',
       configuration: {}
     }
   }
